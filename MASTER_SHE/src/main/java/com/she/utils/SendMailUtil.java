89d7fbc91d6074ae8332e2e6ccb0abb6684d6021
wany <bosswany@daum.net>
2024-06-11 17:09:05 +0900

    google smtp 추가

--- MASTER_SHE/src/main/java/com/she/utils/SendMailUtil.java
+++ MASTER_SHE/src/main/java/com/she/utils/SendMailUtil.java
@@ -26,8 +26,12 @@ public class SendMailUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(SendMailUtil.class);
 
-    private static final String SMTP_HOST_NAME = "203.241.0.125";
-    private static final String SMTP_PORT = "25";
+    private static final String SMTP_HOST_NAME = "smtp.gmail.com";
+    private static final String SMTP_PORT = "465";
+    private static final String SMTP_HOST_ID = "bosswany@gmail.com";
+    private static final String SMTP_HOST_PASSWORD = "iyrqgrjwhceeryjn";
+    private static final String SMTP_ENCODING = "UTF-8";
+    private final static String systemName = "[안전보건 관리시스템]";
 
     /**
      * 파일내용가져오기
@@ -95,18 +99,23 @@ public class SendMailUtil {
             Reader reader = Resources.getResourceAsReader(resource);
             properties.load(reader);
             String active = properties.getProperty("spring.profiles.active");
+            System.out.println("===active===" + active);
 
+            // if (!"prd".equals(active)) {
             if ("dev".equals(active)) {
                 result.setResultCd("SUCCESS"); // 결과코드
                 result.setResultMsg("개발은 메일 서비스 이용에 제한됩니다."); // 결과메세지
             } else {
                 try {
                     Properties props = new Properties();
+                    props.put("mail.use", "true");
                     props.put("mail.smtp.host", SMTP_HOST_NAME);
                     props.put("mail.smtp.port", SMTP_PORT);
+                    props.put("mail.host.auth.flag", "true");
+                    props.put("mail.smtp.encoding", SMTP_ENCODING);
                     Session session = Session.getInstance(props, new javax.mail.Authenticator() {
                         protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication("", "");
+                            return new PasswordAuthentication(SMTP_HOST_ID, SMTP_HOST_PASSWORD);
                         }
                     });
 
@@ -120,15 +129,12 @@ public class SendMailUtil {
                     int i = 0;
                     for (String recipient : recipients) {
                         addressTo[i] = new InternetAddress(recipient);
-                        // addressTo[i] = new
-                        // InternetAddress("pe911642@poscoict.com");
                         i++;
                     }
                     msg.setRecipients(Message.RecipientType.TO, addressTo);
+                    msg.setSubject(systemName + " " + mailVo.getTitle(), "utf-8");
                     msg.setSubject(mailVo.getTitle(), "utf-8");
                     msg.setContent(makeMailContent(mailVo.getContents()), "text/html;charset=euc-kr");
-                    // msg.setContent(mailVo.getContents(),
-                    // "text/html;charset=euc-kr");
                     msg.setHeader("Content-Transfer-Encoding", "base64");
 
                     Transport transport = session.getTransport();
